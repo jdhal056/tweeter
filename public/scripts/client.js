2ed716ad4aab8abdd81e3ee696ae4d92cab343fa
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-25 19:16:46 +0000

    implement function to render tweets dynamicaly

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,36 @@
-function createTweetElement (tweetData) {
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $(".tweet-container").prepend($tweet);
+  }
+}
+
+const createTweetElement = function(tweetData) {
   const $tweet = $("<article>").addClass("tweet");
   let html = 
   `<header>
@@ -25,21 +57,4 @@ function createTweetElement (tweetData) {
   return $tweet;
 }
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
